20d3f9f8fea6aaedc3c958df1f44cf2486dec4ab
carolinehagen <107929032+carolinehagen@users.noreply.github.com>
2022-12-12 12:00:44 -0500

    Update game3.c
    Formatting changes

--- game3.c
+++ game3.c
@@ -2,7 +2,8 @@
 #include <time.h>
 
 /* 
-	This is code to solve a puzzle given to me by work. This is the puzzle:
+	This is code to solve a puzzle. 
+	This is the puzzle:
 
 		       35
 	_  _  _  _  _ |33
@@ -22,7 +23,7 @@
 
 
 	This code finds 53,826 correct matrices 
-	This code takes 12 seconds to run. 
+	This code takes 11 seconds to run. 
 
 */
 
@@ -33,42 +34,14 @@ int main()
 	int x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25;
 	
 	// x3, x13, x18 can't be less than 2 because x3+x8+x13+x18+5 = 34 which can't be true if any of them are less than 2
-	x3 = 2;
-	x13 = 2;
-	x18 = 2;
-
 	// x17 can't be less than 2 because x21+x17+x13+x9+x5 = 35 and x21 is never greater than 6, so none of the other 4 can be less than 2 
-	x17 = 2;
-	
 	// x5, x10, x15, x20 can't be less than 7 because x5+x10+x15+x20+5 = 39 which can't be true if any of them are less than 7
-	x5 = 7;
-	x10 = 7;
-	x15 = 7;
-	x20 = 7;
-
 	// x6-x10 can't be less than 6 because x6+x7+x8+x9+x10 = 42 which can't be true if any of them are less than 6 
-	x6 = 6; 
-	x7 = 6;
-	x8 = 6;
-	x9 = 6;
-
-	x1 = 1;
-	x2 = 1;
-	x4 = 1;
-	x11 = 1;
-	x12 = 1;
-	x14 = 1;
-	x16 = 1;
-	
-	x19 = 1;
-	x21 = 1;
-	x22 = 1;
-	x24 = 1;
 
 	x23 = 5;
 	x25 = 5;
 
-	int y1, y2, y3, y4, y5, y6, y7, y8, y9, y10;
+	int y1, y2, y3, y4, y5, y6, y7, y8, y9, y10 ;
 	int dh, dl;
 
 	y1 = 33;
@@ -88,91 +61,91 @@ int main()
 	int total = 0;
 
 
-	for (x1; x1<10; x1++)
+	for (x1 = 1; x1<10; x1++)
 	{
-		for(x2; x2<10; x2++)
+		for(x2 = 1; x2<10; x2++)
 		{
-			for (x3; x3<10; x3++)
+			for (x3 = 2; x3<10; x3++)
 			{
-				for (x4; x4<10; x4++)
+				for (x4 = 1; x4<10; x4++)
 				{
 					if((x1+x2+x3+x4) >= y1)
 					{
 						break;
 					}
-					for (x5; x5<10; x5++)
+					for (x5 = 7; x5<10; x5++)
 					{
 						if((x1+x2+x3+x4+x5) == y1)
 						{
-							for (x6; x6<10; x6++)
+							for (x6 = 6; x6<10; x6++)
 							{
-								for (x7; x7<10; x7++)
+								for (x7 = 6; x7<10; x7++)
 								{
-									for (x8; x8<10; x8++)
+									for (x8 = 6; x8<10; x8++)
 									{
-										for (x9; x9<10; x9++)
+										for (x9 = 6; x9<10; x9++)
 										{
-											for (x10; x10<10; x10++)
+											for (x10 = 7; x10<10; x10++)
 											{
 												if((x6+x7+x8+x9+x10) == y2)
 												{
-													for (x11; x11<10; x11++)
+													for (x11 = 1; x11<10; x11++)
 													{
-														for (x12; x12<10; x12++)
+														for (x12 = 1; x12<10; x12++)
 														{
 															if((x2+x7+x12) >= y7)
 															{
 																break;
 															}
-															for (x13; x13<10; x13++)
+															for (x13 = 2; x13<10; x13++)
 															{
 																if((x1+x7+x13+x25)>= dl)
 																{
 																	break;
 																}
-																for (x14; x14<10; x14++)
+																for (x14 = 1; x14<10; x14++)
 																{
 																	if((x11+x12+x13+x14) >= y3)
 																	{
 																		break;
 																	}
-																	for (x15; x15<10; x15++)
+																	for (x15 = 7; x15<10; x15++)
 																	{
 																		if((x11+x12+x13+x14+x15) == y3)
 																		{
-																			for (x16; x16<10; x16++)
+																			for (x16 = 1; x16<10; x16++)
 																			{
 																				if((x1+x6+x11+x16) >= y6)
 																				{
 																					break;
 																				}
-																				for (x17; x17<10; x17++)
+																				for (x17 = 2; x17<10; x17++)
 																				{
 																					if((x2+x7+x12+x17) >= y7)
 																					{
 																						break;
 																					}
-																					for (x18; x18<10; x18++)
+																					for (x18 = 2; x18<10; x18++)
 																					{
 																						if((x3+x8+x13+x18+x23) == y8)
 																						{
-																							for (x19; x19<10; x19++)
+																							for (x19 = 1; x19<10; x19++)
 																							{
 																								if((x1+x7+x13+x19+x25) == dl)
 																								{
-																									for (x20; x20<10; x20++)
+																									for (x20 = 7; x20<10; x20++)
 																									{
 																										if((x5+x10+x15+x20+x25) == y10)
 																										{
 																											if((x16+x17+x18+x19+x20) == y4)
 																											{
-																												for (x21; x21<7; x21++)
+																												for (x21 = 1; x21<7; x21++)
 																												{
 																													if((x1+x6+x11+x16+x21) == y6)
 																													{
 																														if((x21+x17+x13+x9+x5) == dh)
 																														{
-																															for (x22; x22<7; x22++)
+																															for (x22 = 1; x22<7; x22++)
 																															{
 																																if((x21+x22+x23+x25) >= y5)
 																																{
@@ -180,7 +153,7 @@ int main()
 																																}
 																																if((x2+x7+x12+x17+x22) == y7)
 																																{
-																																	for (x24; x24<7; x24++)
+																																	for (x24 = 1; x24<7; x24++)
 																																	{																																																																			
 																																		if(((x4+x9+x14+x19+x24) == y9))
 																																		{																																																																																																							
@@ -195,55 +168,32 @@ int main()
 																													}
 																												}
 																											}
-																											x24 = 1;
 																										}
-																										x22 = 1;
 																									}
-																									x21 = 1;
 																								}
 																							}
-																							x20 = 7;
 																						}
-																						x19 = 1;
 																					}
-																					x18 = 2;
 																				}
-																				x17 = 2;
 																			}
-																			x16 = 1;
 																		}
 																	}
-																	x15 = 7;
 																}
-																x14 = 1;
 															}
-															x13 = 2;
 														}
-														x12 = 1;
 													}
-													x11 = 1;
 												}
 											}
-											x10 = 7;
 										}
-										x9 = 6;
 									}
-									x8 = 6;
 								}
-								x7 = 6;
 							}
-							x6 =6;
 						}
 					}
-					x5 = 7;
 				}
-				x4 = 1;
 			}
-			x3 = 2;
 		}
-		x2 = 1;
 	}
-	x1 = 1;
 
 printf("total correct matrices: %d\n", total);
 time(&end);
