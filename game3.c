5ef4158f1e489c7b101b660c06bf1b3472935eb6
carolinehagen <107929032+carolinehagen@users.noreply.github.com>
2022-12-12 07:45:09 -0500

    Add files via upload

---
+++ game3.c
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#include <time.h>
+
+/* 
+	This is code to solve a puzzle given to me by work. This is the puzzle:
+
+			       35
+	_  _  _  _  _ |33
+	_  _  _  _  _ |42
+	_  _  _  _  _ |28
+	_  _  _  _  _ |34
+	_  _  5  _  5 |18
+	______________29
+	29 21 34 32 39 
+
+
+	Fill in the missing numbers.
+	The numbers are integers between 0 and 10 (I interpreted this as 1-9 inclusive)
+	The numbers in each row add up to the totals on the right 
+	The numbers in each column add up to the totals along the bottom
+	The diagnol lines also add up to the totals on the right 
+
+
+	This code finds 53,826 correct matrices 
+	This code takes 72 seconds to run. 
+
+*/
+
+int main()
+{
+	time_t start, end;
+	time(&start);
+	int x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25;
+	x1 = 1;
+	x2 = 1;
+	x3 = 2;
+	x4 = 1;
+	x5 = 7;
+	x6 = 6;
+	x7 = 6;
+	x8 = 6;
+	x9 = 6;
+	x10 = 7;
+	x11 = 1;
+	x12 = 1;
+	x13 = 2;
+	x14 = 1;
+	x15 = 7;
+	x16 = 1;
+	x17 = 1;
+	x18 = 2;
+	x19 = 1;
+	x20 = 7;
+	x21 = 1;
+	x22 = 1;
+	x24 = 1;
+	int y1, y2, y3, y4, y5, y6, y7, y8, y9, y10;
+	int dh, dl;
+
+	x23 = 5;
+	x25 = 5;
+	
+	y1 = 33;
+	y2 = 42;
+	y3 = 28;
+	y4 = 34;
+	y5 = 18;
+	y6 = 29;
+	y7 = 21;
+	y8 = 34;
+	y9 = 32;
+	y10 = 39;
+
+	dh = 35;
+	dl = 29;
+
+	int total = 0;
+
+
+	for (x1; x1<10; x1++)
+	{
+		for(x2; x2<10; x2++)
+		{
+			for (x3; x3<10; x3++)
+			{
+				for (x4; x4<10; x4++)
+				{
+					for (x5; x5<10; x5++)
+					{
+						if((x1+x2+x3+x4+x5) == y1)
+						{
+							for (x6; x6<10; x6++)
+							{
+								for (x7; x7<10; x7++)
+								{
+									for (x8; x8<10; x8++)
+									{
+										for (x9; x9<10; x9++)
+										{
+											for (x10; x10<10; x10++)
+											{
+												if((x6+x7+x8+x9+x10) == y2)
+												{
+													for (x11; x11<10; x11++)
+													{
+														for (x12; x12<10; x12++)
+														{
+															for (x13; x13<10; x13++)
+															{
+																for (x14; x14<10; x14++)
+																{
+																	for (x15; x15<10; x15++)
+																	{
+																		if((x11+x12+x13+x14+x15) == y3)
+																		{
+																			for (x16; x16<10; x16++)
+																			{
+																				for (x17; x17<10; x17++)
+																				{
+																					for (x18; x18<10; x18++)
+																					{
+																						if((x3+x8+x13+x18+x23) == y8)
+																						{
+																							for (x19; x19<10; x19++)
+																							{
+																								if((x1+x7+x13+x19+x25) == dl)
+																								{
+																									for (x20; x20<10; x20++)
+																									{
+																										if((x5+x10+x15+x20+x25) == y10)
+																										{
+																											if((x16+x17+x18+x19+x20) == y4)
+																											{
+																												for (x21; x21<7; x21++)
+																												{
+																													if((x1+x6+x11+x16+x21) == y6)
+																													{
+																														if((x21+x17+x13+x9+x5) == dh)
+																														{
+																															for (x22; x22<7; x22++)
+																															{
+																																if((x2+x7+x12+x17+x22) == y7)
+																																{
+																																	for (x24; x24<7; x24++)
+																																	{																																																																			
+																																		if((x4+x9+x14+x19+x24) == y9)
+																																		{																																																																																																							
+																																			total = total+1;
+																																			printf("total correct matrices: %d\n", total);
+																																			printf("%d %d %d %d %d \n %d %d %d %d %d \n %d %d %d %d %d \n %d %d %d %d %d \n %d %d %d %d %d \n", x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25);
+																																		}
+																																	}
+																																}
+																															}
+																														}
+																													}
+																												}
+																											}
+																											x24 = 1;
+																										}
+																										x22 = 1;
+																									}
+																									x21 = 1;
+																								}
+																							}
+																							x20 = 7;
+																						}
+																						x19 = 1;
+																					}
+																					x18 = 2;
+																				}
+																				x17 = 1;
+																			}
+																			x16 = 1;
+																		}
+																	}
+																	x15 = 7;
+																}
+																x14 = 1;
+															}
+															x13 = 2;
+														}
+														x12 = 1;
+													}
+													x11 = 1;
+												}
+											}
+											x10 = 7;
+										}
+										x9 = 6;
+									}
+									x8 = 6;
+								}
+								x7 = 6;
+							}
+							x6 =6;
+						}
+					}
+					x5 = 7;
+				}
+				x4 = 1;
+			}
+			x3 = 2;
+		}
+		x2 = 1;
+	}
+	x1 = 1;
+
+printf("done");
+time(&end);
+int time_taken = (end - start);
+printf("The time taken by this program is: %d seconds", time_taken);
+}
